6a3ed2ef0a250eccbd7b7d42452f0252d4d2c4f4
alberto <alberto.alvarez512@gmail.com>
2018-10-25 00:52:28 -0500

    changes in data set and avl

--- Project/src/DataStructures/trees/AVL/AVL.java
+++ Project/src/DataStructures/trees/AVL/AVL.java
@@ -169,32 +169,7 @@ public class AVL<K extends Comparable<K>, V> extends TreesWithRotations<K, V> {
 	}
 	
 	
-	public static void main(String[] args) {
-		AVL<Integer, Integer> avl = new AVL<>();
-		int[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-		for (int i = 0; i < data.length; i++) {
-			avl.insert(data[i], data[i]);			
-		}
-		
-		
-//		avl.remove(1);
-//		avl.remove(3);
-//		avl.remove(5);
-//		avl.remove(2);
-//		avl.remove(6);
-//		avl.remove(4);
-//		
-		avl.inOrden((NodeAVL) avl.getRoot());
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+	
 
 
 }
